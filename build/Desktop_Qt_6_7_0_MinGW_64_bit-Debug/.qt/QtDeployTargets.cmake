339190d2a99841ee36079f7bd5a5341b8724869b
qqqqingmo <2300017776@stu.pku.edu.cn>
2024-05-23 01:52:03 +0800

    Ignore build directory

--- build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_MindfulMeadow_FILE C:/Users/QINGMO/Documents/newqt/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/MindfulMeadow.exe)
-set(__QT_DEPLOY_TARGET_MindfulMeadow_RUNTIME_DLLS D:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.7.0/mingw_64/bin/Qt6Sql.dll;D:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
