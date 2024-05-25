96092b8d55a51137d891b2990ca779c85629db1a
qqqqingmo <2300017776@stu.pku.edu.cn>
2024-05-25 23:49:23 +0800

    need to fix a bug

--- planttree.h
+++ planttree.h
@@ -2,7 +2,7 @@
 #define PLANTTREE_H
 
 #include <QWidget>
-
+#include "matterhandler.h"
 namespace Ui {
 class plantTree;
 }
@@ -17,6 +17,14 @@ public:
 
 private:
     Ui::plantTree *ui;
+    QWidget* mainWindow;
+    MatterHandler* handler;
+
+
+private:
+// override closeEvent to hide the window in windows tray
+void closeEvent(QCloseEvent* event);
+void showEvent(QShowEvent *event);
 };
 
 #endif // PLANTTREE_H
